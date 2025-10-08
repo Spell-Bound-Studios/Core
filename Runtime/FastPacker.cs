13164fccb7f3308ebbe7c7ec5cbdc46b70ef98cb
Judsin1349 <judsin1349@gmail.com>
2025-10-08 09:10:31 -0500

    Created the fast packer static class for primitive types

---
+++ Runtime/FastPacker.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+using UnityEngine;
+
+namespace SpellBound.Core {
+    public static class FastPacker {
+        #region Constants
+
+        public const int Vec3ByteSize = sizeof(float) * 3;
+
+        #endregion
+
+        #region Ints
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WriteInt(ref Span<byte> buffer, int value) {
+            BitConverter.TryWriteBytes(buffer, value);
+            buffer = buffer[sizeof(int)..];
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int ReadInt(ref ReadOnlySpan<byte> buffer) {
+            var value = BitConverter.ToInt32(buffer);
+            buffer = buffer[sizeof(int)..];
+
+            return value;
+        }
+
+        #endregion
+
+        #region Floats
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WriteFloat(ref Span<byte> buffer, float value) {
+            BitConverter.TryWriteBytes(buffer, value);
+            buffer = buffer[sizeof(float)..];
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float ReadFloat(ref ReadOnlySpan<byte> buffer) {
+            var value = BitConverter.ToSingle(buffer);
+            buffer = buffer[sizeof(float)..];
+
+            return value;
+        }
+
+        #endregion
+
+        #region Strings
+
+        public static void WriteString(ref Span<byte> buffer, string value) {
+            value ??= string.Empty;
+            var bytes = Encoding.UTF8.GetBytes(value);
+            WriteInt(ref buffer, bytes.Length);
+            bytes.AsSpan().CopyTo(buffer);
+            buffer = buffer[bytes.Length..];
+        }
+
+        public static string ReadString(ref ReadOnlySpan<byte> buffer) {
+            var len = ReadInt(ref buffer);
+            var value = Encoding.UTF8.GetString(buffer[..len]);
+            buffer = buffer[len..];
+
+            return value;
+        }
+
+        #endregion
+
+        #region Vector3
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WriteVector3(ref Span<byte> buffer, in Vector3 v) {
+            WriteFloat(ref buffer, v.x);
+            WriteFloat(ref buffer, v.y);
+            WriteFloat(ref buffer, v.z);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector3 ReadVector3(ref ReadOnlySpan<byte> buffer) {
+            var x = ReadFloat(ref buffer);
+            var y = ReadFloat(ref buffer);
+            var z = ReadFloat(ref buffer);
+
+            return new Vector3(x, y, z);
+        }
+
+        #endregion
+    }
+}
