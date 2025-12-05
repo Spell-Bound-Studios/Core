bb1a47b8c659168ea69a86dcb5010ced505f0ada
messdp <dpmessenger15@gmail.com>
2025-12-05 15:56:13 -0800

    added back parts of purge

---
+++ Runtime/ObjectPresetDatabase.cs
@@ -0,0 +1,77 @@
+﻿// Copyright 2025 Spellbound Studio Inc.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Spellbound.Core {
+    /// <summary>
+    /// Global database for all objects.
+    /// </summary>
+    public sealed class ObjectPresetDatabase : MonoBehaviour {
+        private readonly Dictionary<string, ObjectPreset> _presets = new();
+        private readonly Dictionary<(string uid, Type moduleType), PresetModule> _moduleLookup = new();
+
+        private void Awake() {
+            DontDestroyOnLoad(gameObject);
+            SingletonManager.RegisterSingleton(this);
+
+            var presets = Resources.LoadAll<ObjectPreset>("");
+
+            foreach (var preset in presets) {
+                if (!_presets.TryAdd(preset.presetUid, preset))
+                    Debug.LogError($"Duplicate procedural object uid {preset.presetUid}");
+
+                foreach (var module in preset.modules) {
+                    if (module == null)
+                        continue;
+
+                    var key = (preset.presetUid, module.GetType());
+
+                    // This should never happen but regardless.
+                    if (!_moduleLookup.TryAdd(key, module)) {
+                        Debug.LogWarning(
+                            $"{preset.name} already has a {module.GetType().Name}; second copy ignored",
+                            preset);
+                    }
+                }
+            }
+        }
+
+        // Public getter for non-Burst systems that still want the preset object.
+        public bool TryGetPreset(string uid, out ObjectPreset preset) {
+            // Guarantee preset is always assigned.
+            if (!string.IsNullOrEmpty(uid))
+                return _presets.TryGetValue(uid, out preset);
+
+            preset = null;
+
+            return false;
+        }
+    }
+
+    public static class ObjectPresetUtils {
+        /// <summary>
+        /// Returns true and the first module of type T if it exists on preset otherwise false.
+        /// </summary>
+        public static bool TryGetModule<T>(this ObjectPreset preset, out T module) where T : PresetModule {
+            if (preset != null) {
+                module = preset.modules.OfType<T>().FirstOrDefault();
+
+                return module != null;
+            }
+
+            module = null;
+
+            return false;
+        }
+
+        public static ObjectPreset ResolvePreset(this string uid) =>
+                !string.IsNullOrEmpty(uid) &&
+                SingletonManager.TryGetSingletonInstance(out ObjectPresetDatabase db) &&
+                db.TryGetPreset(uid, out var preset)
+                        ? preset
+                        : null;
+    }
+}
---
+++ Runtime/SbbData.cs
@@ -0,0 +1,26 @@
+// Copyright 2025 Spellbound Studio Inc.
+
+using System;
+using Spellbound.Core.Packing;
+
+namespace Spellbound.Core {
+    /// <summary>
+    /// 
+    /// </summary>
+    public struct SbbData : IPacker {
+        public string Id;
+        public byte[] PackedData;
+
+        public override string ToString() => $"SbbData ▶ Id={Id}, Bytes={PackedData?.Length ?? 0}";
+
+        public void Pack(ref Span<byte> buffer) {
+            Packer.WriteString(ref buffer, Id);
+            Packer.WriteBytes(ref buffer, PackedData);
+        }
+
+        public void Unpack(ref ReadOnlySpan<byte> buffer) {
+            Id = Packer.ReadString(ref buffer);
+            PackedData = Packer.ReadBytes(ref buffer);
+        }
+    }
+}
