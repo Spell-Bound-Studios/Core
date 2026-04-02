76619074fc3c66cb76d761dc37df979cbfce42d7
messdp <dpmessenger15@gmail.com>
2026-04-02 15:02:13 -0700

    fixing

---
+++ Runtime/ObjectHandling/TransformData.cs
@@ -0,0 +1,31 @@
+﻿// Copyright 2026 Spellbound Studio Inc.
+
+using System;
+using Spellbound.Core.Packing;
+using UnityEngine;
+
+namespace Spellbound.Core {
+    public struct TransformData : IPacker {
+        public Vector3 Position;
+        public Vector3 Rotation;
+        public int Scale;
+
+        public TransformData(Vector3 position, Vector3 rotation, int scale) {
+            Position = position;
+            Rotation = rotation;
+            Scale = scale;
+        }
+        
+        public void Pack(ref Span<byte> buffer) {
+            Packer.WriteVector3(ref buffer, Position);
+            Packer.WriteVector3(ref buffer, Rotation);
+            Packer.WriteInt(ref buffer, Scale);
+        }
+
+        public void Unpack(ref ReadOnlySpan<byte> buffer) {
+            Position = Packer.ReadVector3(ref buffer);
+            Rotation = Packer.ReadVector3(ref buffer);
+            Scale = Packer.ReadInt(ref buffer);
+        }
+    }
+}
