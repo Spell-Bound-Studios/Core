64703f9110b03e36ba7b649f09cafa57e5121768
Judsin <judsin1349@gmail.com>
2026-05-11 16:54:47 -0500

    removed i chunk migration coordinator

--- Runtime/ObjectHandling/IChunkMigrationCoordinator.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright 2026 Spellbound Studio Inc.
-
-namespace Spellbound.Core {
-    public interface IChunkMigrationCoordinator {
-        void RequestMigration(IMigratable migratable);
-    }
-}
